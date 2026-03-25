a5ab45f45e3aa7d2920732adcd6ab6ef80e53edb
Cassandra Bazille <bazillecassandra@gmail.com>
2026-03-25 15:10:10 +0100

    change of split

--- level3/split/split.c
+++ level3/split/split.c
@@ -1,3 +1,24 @@
 #include <stdlib>
 
+char	**ft_split(char *str)
+{
+	char **tab;
+	int i;
+	int j;
+	int k;
 
+	tab = malloc(sizeof(char*) * 1000);
+	if (!tab == NULL)
+		return (NULL);
+	while (str[i] >= '9' && stri[i] <= 13 || str[i] == ' ')
+		i++;
+	while (str[i])
+	{
+		if (str[i] > 32)
+		{
+			tab[j] = malloc (sizeof(char *) * 1000)
+
+
+
+
+}
